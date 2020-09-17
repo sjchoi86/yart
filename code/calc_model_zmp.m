29033e9594daa2c93c4dfb50a2ea64f07a3df541
Hosik Chae <CKMagenta@gmail.com>
2020-09-17 15:55:08 +0900

    updated to avoid vector division

--- code/calc_model_zmp.m
+++ code/calc_model_zmp.m
@@ -3,7 +3,7 @@ function zmp = calc_model_zmp(chain_model,dP,dL,zmp_z)
 M = chain_model.M;
 G = [0,0,-9.8];
 c = chain_model.com;
-zmp_x = (M*G*c(1) + zmp_z*dP(1) - dL(2)) / (M*G + dP(3));   % Kajita (3.73)
-zmp_y = (M*G*c(2) + zmp_z*dP(2) + dL(1)) / (M*G + dP(3));   % Kajita (3.74)
+zmp_x = (M*G(3)*c(1) + zmp_z*dP(1) - dL(2)) / (M*G(3) + dP(3));   % Kajita (3.73)
+zmp_y = (M*G(3)*c(2) + zmp_z*dP(2) + dL(1)) / (M*G(3) + dP(3));   % Kajita (3.74)
 
 zmp = [zmp_x,zmp_y,zmp_z];
